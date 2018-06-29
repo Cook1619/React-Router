d7d8c2089323ad20d5567c01eb7de41572690cfc
Cook1619 <cook6348@gmail.com>
2018-06-29 10:39:52 -0500

    Link is working, just need to connect it to another component

--- src/component/filmDetails.jsx
+++ src/component/filmDetails.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-let filmDetails = ({filmData}) => {
+let filmDetails = ({ filmData }) => {
     return (
         <div className="card">
             <div className="card-header bg-info m-4 col-8 d-flex mx-auto ">
                 <div className="card-body bg-dark text-white">{filmData.title}
-                   </div>
-            </div>
-        </div>
+                    
+                            <Link key={filmData} className="text-light float-right" to={`/film/${filmData.id}`}> More Details</Link>
+                        </div>
+                    </div>
+                </div>
+          
+    
     )
 }
 
